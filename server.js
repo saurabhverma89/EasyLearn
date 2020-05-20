d0e78ef29f88842627ad08517524ee7130d14638
Saurabh Kumar Verma <ssaurabh.verma@hotmail.com>
2020-05-20 23:40:54 +0530

    added db connection

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-// const mongoose = require('mongoose')
-// console.log(process.env.DATABASE_URL)
-// mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
-// const db = mongoose.connection
-// db.on('error', error => console.error(error))
-// db.once('open', () => console.log('Connected to Mongoose'))
+const mongoose = require('mongoose')
+console.log(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 
 app.use('/', indexRouter)
